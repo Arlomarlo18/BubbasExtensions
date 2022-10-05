83cee0925bd0d79396f05a6d72f10160d414409c
Derek Hall <shotgolf34@yahoo.com>
2022-10-05 11:54:53 -0600

    Update IndexMatching.swift
    added a second index matching func

--- Sources/BubbasExtensions/IndexMatching.swift
+++ Sources/BubbasExtensions/IndexMatching.swift
@@ -14,3 +14,27 @@ extension Collection where Element: Identifiable {
         firstIndex(where: { $0.id == element.id })
     }
 }
+
+@available(iOS 13.0, *)
+extension RangeReplaceableCollection where Element: Identifiable {
+    mutating func remove(_ element: Element) {
+        if let index = index(matching: element) {
+            remove(at: index)
+        }
+    }
+
+    subscript(_ element: Element) -> Element {
+        get {
+            if let index = index(matching: element) {
+                return self[index]
+            } else {
+                return element
+            }
+        }
+        set {
+            if let index = index(matching: element) {
+                replaceSubrange(index...index, with: [newValue])
+            }
+        }
+    }
+}
