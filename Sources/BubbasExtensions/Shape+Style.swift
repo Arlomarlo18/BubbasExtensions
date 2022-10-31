c54f1dfff8f5d514bcbd774711b59ddeceee587f
Derek Hall <shotgolf34@yahoo.com>
2022-10-31 13:56:16 -0600

    Create Shape+Style
    Added a new extension on shape

---
+++ Sources/BubbasExtensions/Shape+Style.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bubba Hall on 10/31/22.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+extension Shape {
+    public func fillAndStroke(fill fillContent: some ShapeStyle, strokeColor: some ShapeStyle, strokeWidth: CGFloat) -> some View {
+        self
+            .stroke(strokeColor, lineWidth: strokeWidth)
+            .background(fill(fillContent))
+    }
+}
