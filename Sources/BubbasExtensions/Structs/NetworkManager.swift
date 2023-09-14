56f94d0209d693b5bddf0d7422e75e20530110e0
Bubba Hall <100863550+Arlomarlo18@users.noreply.github.com>
2023-09-14 10:32:21 -0600

    added network manager

---
+++ Sources/BubbasExtensions/Structs/NetworkManager.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkManager.swift
+//  
+//
+//  Created by Bubba Hall on 9/14/23.
+//
+
+import Foundation
+
+@available(iOS 15.0, *)
+public struct NetworkManager {
+   public func fetch<T: Codable>(from urlString: String, results: inout T) async throws {
+        
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+        let result = try JSONDecoder().decode(T.self, from: data)
+        results = result
+    }
+}
+
