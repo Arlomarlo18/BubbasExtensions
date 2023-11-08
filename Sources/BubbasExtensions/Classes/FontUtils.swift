b8a6f1775d63e6244f204bc5e29b3715a9fc9e6e
Bubba Hall <100863550+Arlomarlo18@users.noreply.github.com>
2023-11-08 10:18:47 -0700

    This will add a FontUtils class and the printSystemFonts function

---
+++ Sources/BubbasExtensions/Classes/FontUtils.swift
@@ -0,0 +1,27 @@
+//
+//  FontUtils.swift
+//
+//
+//  Created by Bubba Hall on 11/8/23.
+//
+
+import Foundation
+import SwiftUI
+
+public class FontUtils {
+    /// This static function will print all of the fonts that your project has access to.
+    ///
+    /// If you add a custom font you can call this function and see if it is actually in your project or not.
+    ///
+    /// ```
+    /// FontUtils.printSystemFonts()
+    /// ```
+    public static func printSystemFonts() {
+        for familyName in UIFont.familyNames {
+            print(familyName)
+            for fontName in UIFont.fontNames(forFamilyName: familyName) {
+                print("-- \(fontName)")
+            }
+        }
+    }
+}
