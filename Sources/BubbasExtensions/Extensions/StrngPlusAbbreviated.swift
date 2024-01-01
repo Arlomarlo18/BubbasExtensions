f22ba727feb075758e7ab728a5ba67c7270cea15
Bubba Hall <100863550+Arlomarlo18@users.noreply.github.com>
2023-12-31 17:12:53 -0700

    added the description and fixed the name of abbreviate

---
+++ Sources/BubbasExtensions/Extensions/StrngPlusAbbreviated.swift
@@ -0,0 +1,26 @@
+//
+//  StrngPlusAbbreviated.swift
+//
+//
+//  Created by Bubba Hall on 12/31/23.
+//
+
+import Foundation
+
+extension String {
+    
+    /// This function is called on a string and abbreviates it and returns the first letter of every word and capitalizes it.
+    ///
+    /// This is used to shorten the name of objects,
+    /// ```
+    /// var footballTeamName = "Green Bay Packers"
+    /// var code = footballTeamName.abbreviate()
+    /// print(code) // "GBP
+    /// ```
+    ///  - Returns: An Abbreviated Code.
+    public func abbreviate() -> String {
+        let words = self.components(separatedBy: " ")
+        let capitalizedWords = words.map { $0.capitalized }
+        return capitalizedWords.joined(separator: " ")
+    }
+}
